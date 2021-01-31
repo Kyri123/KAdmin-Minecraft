10b191e736754453698e15694eae4d2074c7025f
Kyrium <olli143@live.de>
2021-01-31 11:31:23 +0100

    Vorbereitung für Spielerliste

--- public/js/kadmin/servercenter.js
+++ public/js/kadmin/servercenter.js
@@ -25,14 +25,17 @@ setInterval(() => {
     getSCState()
 }, 1000)
 
+/**
+ * Erstellt Informationen über den Server und trägt die Daten ins Frontend ein
+ */
 function getSCState() {
     $.get('/ajax/serverCenterAny', {
         "getserverinfos": true,
         "server": varser.cfg
     }, (data) => {
-        let serverInfos = JSON.parse(data)
-        let state_id = $('#state')
-        let player_id = $('#player')
+        let serverInfos     = JSON.parse(data)
+        let state_id        = $('#state')
+        let player_id       = $('#player')
         let inhalt
 
         let stateColor                                                 = "danger"
@@ -43,80 +46,86 @@ function getSCState() {
         let stateText = varser.lang_arr.forservers.state[stateColor]
 
         // Versionserfassung
-        let version
-        if(stateColor === "danger" || stateColor === "warning") {
-            version = hasPermissions(globalvars.perm, "versionpicker", varser.cfg)
-               ? `<a href="javascript:void()" class="small-box-footer btn btn-sm btn-success" data-toggle="modal" data-target="#versionpicker">${serverInfos.version}</a>`
-               : serverInfos.version
-        }
-        else {
-            version = serverInfos.version
-        }
-        if($('#version').html().trim().toUpperCase() !== version.trim().toUpperCase()) $('#version').html(version)
+            let version
+            if(stateColor === "danger" || stateColor === "warning") {
+                version = hasPermissions(globalvars.perm, "versionpicker", varser.cfg)
+                   ? `<a href="javascript:void()" class="small-box-footer btn btn-sm btn-success" data-toggle="modal" data-target="#versionpicker">${serverInfos.version}</a>`
+                   : serverInfos.version
+            }
+            else {
+                version = serverInfos.version
+            }
+            if($('#version').html().trim().toUpperCase() !== version.trim().toUpperCase()) $('#version').html(version)
 
         //server IMG
-        $('#serv_img').attr('class', `border-${stateColor}`)
+            $('#serv_img').attr('class', `border-${stateColor}`)
 
         // Status
-        if(state_id.html() !== stateText) state_id.html(stateText).attr('class',`description-header text-${stateColor}`)
-
-        // Spieler
-        if(stateColor === "success") {
-            $('#btnJoin').attr('href', joinAdress).toggleClass("disabled", false)
-            inhalt = `${serverInfos.aplayers} / ${serverInfos.players}`
-            //inhalt = `<a href="#" data-toggle="modal" data-target="#playerlist_modal" class="btn btn-sm btn-primary">${serverInfos.aplayers} / ${serverInfos.players}</a>`
-        }
-        else {
-            $('#btnJoin').attr('href', '').toggleClass("disabled", true)
-            inhalt = `${serverInfos.aplayers} / ${serverInfos.players}`
-        }
-        if(player_id.html() !== inhalt) player_id.html(inhalt)
+            if(state_id.html() !== stateText) state_id.html(stateText).attr('class',`description-header text-${stateColor}`)
 
         // Action Card
-        let css
-        if(stateColor === "warning") {
-            inhalt = varser.lang_arr.servercenter_any.actionClose
-        }
-        else {
-            inhalt = hasPermissions(globalvars.perm, "actions", varser.cfg)
-                ? `<a href="javascript:void()" class="small-box-footer btn btn-sm btn-success" data-toggle="modal" data-target="#action">${varser.lang_arr.servercenter_any.actionFree}</a>`
-                : varser.lang_arr.servercenter_any.actionClose
-        }
-        css = 'success'
-        if($('#actions').html() !== inhalt) $('#actions').html(inhalt).attr('class',`description-header text-${css}`);// Action Card -> Select
+            let css
+            if(stateColor === "warning") {
+                inhalt = varser.lang_arr.servercenter_any.actionClose
+            }
+            else {
+                inhalt = hasPermissions(globalvars.perm, "actions", varser.cfg)
+                    ? `<a href="javascript:void()" class="small-box-footer btn btn-sm btn-success" data-toggle="modal" data-target="#action">${varser.lang_arr.servercenter_any.actionFree}</a>`
+                    : varser.lang_arr.servercenter_any.actionClose
+            }
+            css = 'success'
+            if($('#actions').html() !== inhalt) $('#actions').html(inhalt).attr('class',`description-header text-${css}`);// Action Card -> Select
+
+        // Spielerliste
+            // Button & Anzeige
+            if(stateColor === "success") {
+                $('#btnJoin').attr('href', joinAdress).toggleClass("disabled", false)
+                inhalt = `${serverInfos.aplayers} / ${serverInfos.players}`
+                //inhalt = `<a href="#" data-toggle="modal" data-target="#playerlist_modal" class="btn btn-sm btn-primary">${serverInfos.aplayers} / ${serverInfos.players}</a>`
+            }
+            else {
+                $('#btnJoin').attr('href', '').toggleClass("disabled", true)
+                inhalt = `${serverInfos.aplayers} / ${serverInfos.players}`
+            }
+            if(player_id.html() !== inhalt) player_id.html(inhalt)
+
+            // Liste
+            console.log(serverInfos)
+
 
         // Alerts
-        /*if(serverInfos.alerts !== undefined) {
-            $.get('/json/steamAPI/mods.json', (mods) => {
-                let modNeedUpdates      = []
-                let rplf                = []
-                let tplt                = []
-                mods.response.publishedfiledetails.forEach((val) => {
-                    rplf.push(val.publishedfileid)
-                    tplt.push(`<b>[${val.publishedfileid}]</b> ${val.title}`)
+            /*if(serverInfos.alerts !== undefined) {
+                $.get('/json/steamAPI/mods.json', (mods) => {
+                    let modNeedUpdates      = []
+                    let rplf                = []
+                    let tplt                = []
+                    mods.response.publishedfiledetails.forEach((val) => {
+                        rplf.push(val.publishedfileid)
+                        tplt.push(`<b>[${val.publishedfileid}]</b> ${val.title}`)
+                    })
+                    let list = []
+
+                    let counter = 0
+                    serverInfos.alerts.forEach((val) => {
+                        if(!(val === "3997" && modNeedUpdates.length === 0)) {
+                            list.push(alerter(val, "", 3, false, 3, 3, 3, true))
+                            counter++
+                        }
+                    })
+
+                    $(`#infoCounter`).html(counter)
+                    if(counter === 0) list.push(alerter(4000, "", 3, false, 3, 3, 3, true))
+
+                    $(`#AlertBody`).html(list.join('<hr class="m-0">')
+                        .replace("{modu}", modNeedUpdates.join("</li><li>"))
+                        .replace("{modi}", serverInfos.notInstalledMods.join("</li><li>"))
+                        .replaceArray(rplf, tplt))
                 })
-                let list = []
-
-                let counter = 0
-                serverInfos.alerts.forEach((val) => {
-                    if(!(val === "3997" && modNeedUpdates.length === 0)) {
-                        list.push(alerter(val, "", 3, false, 3, 3, 3, true))
-                        counter++
-                    }
-                })
-
-                $(`#infoCounter`).html(counter)
-                if(counter === 0) list.push(alerter(4000, "", 3, false, 3, 3, 3, true))
-
-                $(`#AlertBody`).html(list.join('<hr class="m-0">')
-                    .replace("{modu}", modNeedUpdates.join("</li><li>"))
-                    .replace("{modi}", serverInfos.notInstalledMods.join("</li><li>"))
-                    .replaceArray(rplf, tplt))
-            })
-        }*/
+            }*/
     })
 }
 
+// Wenn Aktion Modal geöffnet wird
 $('#action').on('show.bs.modal', () => {
     // Selects
     $.get('/json/sites/serverCenterActions.cfg.json' , (datas) => {
@@ -166,6 +175,7 @@ $("#action_form").submit(() => {
     return false
 })
 
+// Parameter erstellen
 $('#action_sel').change(() => {
     var action = $("#action_sel").val()
     $.get('/json/sites/serverCenterActions.cfg.json', (datas) => {
@@ -188,25 +198,29 @@ $('#action_sel').change(() => {
 
 // von: https://gist.github.com/anazard/d42354f45e172519c0be3cead34fe869
 // {
-var $body = document.getElementsByTagName('body')[0]
-var $btnCopy = document.getElementById('btnCopy')
-var secretInfo = document.getElementById('secretInfo').innerHTML
+    let $body               = document.getElementsByTagName('body')[0]
+    let $btnCopy            = document.getElementById('btnCopy')
+    let secretInfo          = document.getElementById('secretInfo').innerHTML
 
-var copyToClipboard = (secretInfo) => {
-    var $tempInput = document.createElement('INPUT')
-    $body.appendChild($tempInput)
-    $tempInput.setAttribute('value', secretInfo)
-    $tempInput.select()
-    document.execCommand('copy')
-    $body.removeChild($tempInput)
-}
+    const copyToClipboard   = (secretInfo) => {
+        let $tempInput      = document.createElement('INPUT')
+        $body.appendChild($tempInput)
+        $tempInput.setAttribute('value', secretInfo)
+        $tempInput.select()
+        document.execCommand('copy')
+        $body.removeChild($tempInput)
+    }
 
-$btnCopy.addEventListener('click', (ev) => {
-    copyToClipboard(secretInfo)
-    alert("Kopiert: " + secretInfo)
-})
+    $btnCopy.addEventListener('click', (ev) => {
+        copyToClipboard(secretInfo)
+        alert("Kopiert: " + secretInfo)
+    })
 // }
 
+/**
+ * Erstellt Versionsliste & Modpack tool
+ * @param qid ID der Quelle (Select)
+ */
 function generateVersionList(qid) {
     let quelle      = $(`#${qid}`)
     let ziel        = $(`#vpick`)
